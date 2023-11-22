93326018c98e998f041f200c3e1dd292c5069bd9
JosephKagwi <josephkagwi28@gmail.com>
2023-11-22 12:54:36 +0300

    Assignment complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        result = []
+    elif length == 1:
+        result = [0]
+    elif length == 2:
+        result = [0, 1]
+    else:
+        fibonacci_sequence = [0, 1]
+        for i in range(2, length):
+            next_fib = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+            fibonacci_sequence.append(next_fib)
+        result = fibonacci_sequence
+    
+    print(result) 
+    return result if length > 0 else "[]" 
+
